5feb36b1a84f4490f037aefb3a216daa55d70b80
iqpizza6349 <iqpizza6349@gmail.com>
2023-11-13 12:04:23 +0900

    :tada: start recom version 2

---
+++ src/main/java/com/pandabear/recom/RecomServerV2Application.java
@@ -0,0 +1,13 @@
+package com.pandabear.recom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecomServerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pandabear/recom/RecomServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandabear.recom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomServerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
